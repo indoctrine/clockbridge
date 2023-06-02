b80f9c54792175ba279781299ecb0195ba6b98ea
Meta <meta@gitlab.home.bsdgirl.net>
2023-06-02 12:22:31 +1000

    Converted current test cases to pytest library

--- tests/TestConfigurationClass.py
+++ tests/TestConfigurationClass.py
@@ -1,32 +1,34 @@
-import unittest
+import pytest
 import yaml
+import sys
 import os
 import json
 from io import StringIO
+sys.path.append(os.path.abspath('../'))
 from clockbridgeconfig import Config
 
-class TestLoadConfigFile(unittest.TestCase):
-    def setUp(self):
+class TestLoadConfigFile:
+    def setup_class(self):
         self.config = Config()
 
     def test_invalid_path(self):
         """ Test for nonexistent/unreadable file """
-        with self.assertRaises(IOError) as exception_context:
+        with pytest.raises(IOError) as exception_context:
             self.config.load_config_file('nonexistent.yaml')
         
     def test_nonfile_path(self):
         """ Test for file being a directory """
-        with self.assertRaises(IsADirectoryError) as exception_context:
+        with pytest.raises(IsADirectoryError) as exception_context:
             self.config.load_config_file(os.getcwd())
 
-class TestParseConfigFile(unittest.TestCase):
-    def setUp(self):
+class TestParseConfigFile:
+    def setup_class(self):
         self.config = Config()
     
     def test_invalid_config_file(self):
         """Test whether a valid non-YAML file is YAML"""
         invalid_config_file = StringIO("Not a real YAML file")
-        with self.assertRaises(yaml.YAMLError) as exception_context:
+        with pytest.raises(yaml.YAMLError) as exception_context:
             self.config._Config__parse_config_file(invalid_config_file)
     
     def test_invalid_config_schema(self):
@@ -36,7 +38,7 @@ config:
     sheets-creds: 
         location: config.yaml
         """)
-        with self.assertRaises(yaml.YAMLError) as exception_context:
+        with pytest.raises(yaml.YAMLError) as exception_context:
             self.config._Config__parse_config_file(invalid_config_file)
 
     def test_valid_config_file(self):
@@ -50,30 +52,30 @@ config:
     sheets-creds: 
         location: config.yaml
         """)
-        self.assertIsInstance(self.config._Config__parse_config_file(valid_config_file), dict)
+        assert type(self.config._Config__parse_config_file(valid_config_file)) is dict
 
-class TestLoadSheetsCreds(unittest.TestCase):
-    def setUp(self):
+class TestLoadSheetsCreds:
+    def setup_class(self):
         self.config = Config()
 
     def test_invalid_creds_file(self):
         """ Test for nonexistent/unreadable file """
-        with self.assertRaises(IOError) as exception_context:
+        with pytest.raises(IOError) as exception_context:
             self.config.load_sheets_creds('nonexistent.json')
         
     def test_creds_nonfile(self):
         """ Test for file being a directory """
-        with self.assertRaises(IsADirectoryError) as exception_context:
+        with pytest.raises(IsADirectoryError) as exception_context:
             self.config.load_sheets_creds(os.getcwd())
 
-class TestValidateSheetsCreds(unittest.TestCase):
-    def setUp(self):
+class TestValidateSheetsCreds:
+    def setup_class(self):
         self.config = Config()
     
     def test_invalid_creds_file(self):
         """Test whether a valid non-JSON file is JSON"""
         invalid_creds_file = StringIO("Not a real JSON file")
-        with self.assertRaises(json.decoder.JSONDecodeError) as exception_context:
+        with pytest.raises(json.decoder.JSONDecodeError) as exception_context:
             self.config._Config__validate_sheets_creds(invalid_creds_file)
     
     def test_invalid_creds_schema(self):
@@ -83,7 +85,7 @@ class TestValidateSheetsCreds(unittest.TestCase):
                 "project_id": 123,
                 "testing": "test",
             }""")
-        with self.assertRaises(json.decoder.JSONDecodeError) as exception_context:
+        with pytest.raises(json.decoder.JSONDecodeError) as exception_context:
             self.config._Config__validate_sheets_creds(invalid_creds_file)
 
     def test_valid_creds_file(self):
@@ -101,8 +103,4 @@ class TestValidateSheetsCreds(unittest.TestCase):
                 "client_x509_cert_url": "https://example.com/client/x509"
             }
         """)
-        self.assertIsInstance(self.config._Config__validate_sheets_creds(valid_creds_file), dict)
-
-if __name__ == '__main__':
-    import xmlrunner
-    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-config-report'))
+        assert type(self.config._Config__validate_sheets_creds(valid_creds_file)) is dict
