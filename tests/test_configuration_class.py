9dc49ddee3bb682646b2d9e1235fcd9979e65f62
Meta <meta@gitlab.home.bsdgirl.net>
2023-08-14 21:09:54 +1000

    Linting and better assertion code for type checking

--- tests/test_configuration_class.py
+++ tests/test_configuration_class.py
@@ -9,30 +9,33 @@ sys.path.append(os.path.abspath('../'))
 
 config_path = os.environ.get('CLOCKBRIDGE_CONFIG_PATH')
 if not config_path:
-    raise ValueError('CONFIG_FILE_PATH environment variable is not set')
+    raise ValueError('CONFIG_FILE_PATH environment variable is not set - please set the location of the configuration file')
 
 class TestLoadConfigFile:
+    """ Test methods related to loading the configuration file """
     def test_invalid_path(self):
         """ Test for nonexistent/unreadable file """
         with pytest.raises(IOError):
             self.config = Config('nonexistent.yaml')
-        
+
     def test_nonfile_path(self):
         """ Test for file being a directory """
         with pytest.raises(IsADirectoryError):
             self.config = Config(os.getcwd())
 
 class TestParseConfigFile:
+    """ Test methods related to parsing the configuration file """
     def setup_class(self):
         self.config = Config(config_path)
-    
+
     def test_invalid_config_file(self):
         """Test whether a valid non-YAML file is YAML"""
         invalid_config_file = StringIO("Not a real YAML file")
         with pytest.raises(yaml.YAMLError):
             self.config._Config__parse_config_file(invalid_config_file)
-    
+
     def test_invalid_config_schema(self):
+        """Test a valid YAML file in incorrect schema"""
         invalid_config_file = StringIO("""
 config: 
     webhook_secrets: xxx
@@ -43,6 +46,7 @@ config:
             self.config._Config__parse_config_file(invalid_config_file)
 
     def test_valid_config_file(self):
+        """Test a valid YAML file in the correct schema returns the expected data structures"""
         valid_config_file = StringIO("""
 config: 
     webhook_secrets: 
@@ -53,12 +57,14 @@ config:
     sheets_creds: 
         location: testSecrets.json
         """)
-        assert type(self.config._Config__parse_config_file(valid_config_file)) is bool 
-        assert type(self.config.webhook_secrets) is list
-        assert type(self.config.sheets_map) is list
-        assert type(self.config.sheets_creds) is dict
+
+        assert isinstance(self.config._Config__parse_config_file(valid_config_file), bool)
+        assert isinstance(self.config.webhook_secrets, list)
+        assert isinstance(self.config.sheets_map, list)
+        assert isinstance(self.config.sheets_creds, dict)
 
 class TestLoadSheetsCreds:
+    """ Test methods related to loading the Google Sheets credentials file """
     def setup_class(self):
         self.config = Config(config_path)
 
@@ -66,13 +72,14 @@ class TestLoadSheetsCreds:
         """ Test for nonexistent/unreadable file """
         with pytest.raises(IOError):
             self.config.load_sheets_creds('nonexistent.json')
-        
+
     def test_creds_nonfile(self):
         """ Test for file being a directory """
         with pytest.raises(IsADirectoryError):
             self.config.load_sheets_creds(os.getcwd())
 
 class TestValidateSheetsCreds:
+    """ Test methods related to validating the Google Sheets credentials file """
     def setup_class(self):
         self.config = Config(config_path)
 
@@ -81,8 +88,9 @@ class TestValidateSheetsCreds:
         invalid_creds_file = StringIO("Not a real JSON file")
         with pytest.raises(json.decoder.JSONDecodeError):
             self.config._Config__validate_sheets_creds(invalid_creds_file)
-    
+
     def test_invalid_creds_schema(self):
+        """Test a valid JSON file in incorrect schema"""
         invalid_creds_file = StringIO("""
         {
                 "type": "xxxx",
@@ -93,6 +101,7 @@ class TestValidateSheetsCreds:
             self.config._Config__validate_sheets_creds(invalid_creds_file)
 
     def test_valid_creds_file(self):
+        """Test a valid JSON file in the correct schema returns the expected data structures"""
         valid_creds_file = StringIO("""
             {
                 "type": "test",
@@ -107,4 +116,4 @@ class TestValidateSheetsCreds:
                 "client_x509_cert_url": "https://example.com/client/x509"
             }
         """)
-        assert type(self.config._Config__validate_sheets_creds(valid_creds_file)) is dict
+        assert isinstance(self.config._Config__validate_sheets_creds(valid_creds_file), dict)
