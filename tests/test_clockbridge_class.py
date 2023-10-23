9c9009523af4d4dd41957dbd19f38f18a0cbdf19
Meta <meta@gitlab.home.bsdgirl.net>
2023-10-23 20:31:42 +1100

    Add test cases for payload verification

--- tests/test_clockbridge_class.py
+++ tests/test_clockbridge_class.py
@@ -1,7 +1,6 @@
 import sys
 import os
-from io import StringIO
-import pytest
+import json
 from clockbridgeconfig import Config
 from clockbridge import Clockbridge
 sys.path.append(os.path.abspath('../'))
@@ -43,3 +42,42 @@ class TestVerifyWebhookSignature:
         """ Test whether using something other than a dictionary handles gracefully """
         headers = []
         assert self.bridge.verify_webhook_signature(headers) is False
+
+class TestVerifyWebhookPayload:
+    """ Test methods related to verifying the webhook payload """
+    def setup_class(self):
+        self.config = Config(config_path)
+        self.bridge = Clockbridge(self.config)
+        json_data = """
+        {
+            "description": "",
+            "id": "",
+            "project": {
+                "clientId": "",
+                "clientName": "",
+                "estimate": {
+                    "estimate": "PT0S",
+                    "type": "AUTO"
+                },
+                "name": "",
+                "workspaceId": ""
+            },
+            "projectId": "",
+            "timeInterval": {
+                "duration": "",
+                "end": "",
+                "start": ""
+            },
+            "userId": ""
+        }
+        """
+        self.payload = json.loads(json_data)
+
+    def test_no_payload(self):
+        """ Test for nonexistent signature """
+        payload = {}
+        assert self.bridge.verify_webhook_payload(payload) is False
+
+    def test_required_fields_null(self):
+        """ Test for required fields being null"""
+        assert self.bridge.verify_webhook_payload(self.payload) is False
