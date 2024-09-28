3c1529de4b51d314ac702699bf23928000d41897
Meta <meta@gitlab.home.bsdgirl.net>
2024-09-28 12:26:41 +1000

    Additional logging

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
+import sys
+import logging
 from clockbridgeconfig import Config
 import webhook
 from flask import Flask, Response, request
-import sys
-import logging
 
 # Kludge libraries
 import requests
@@ -11,15 +11,18 @@ import urllib3
 from datetime import datetime, timezone
 import json
 
-logger = logging.getLogger(__name__)
-logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s',
-                    stream=sys.stderr,level=logging.INFO)
 file_path = os.environ.get('CLOCKBRIDGE_CONFIG_PATH')
 if not file_path:
     file_path = os.path.join(os.getcwd(), 'config.yaml')
 
 app = Flask(__name__)
 config = Config(file_path)
+logging.info("Configuration loaded from %s, logging at %s level", file_path, config.log_level)
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s',
+                    stream=sys.stderr)
+logger.setLevel(config.log_level)
 
 @app.route("/ping", methods = ['GET'])
 def ping():
@@ -28,9 +31,10 @@ def ping():
 @app.route("/webhook/clockify", methods = ['POST'])
 def clockbridge():
     try:
+        logger.info("Incoming payload")
         hook = webhook.Webhook(config)
-        logging.info("Payload headers:\n %s", dict(request.headers))
-        logging.info("Processing payload:\n %s", json.dumps(request.json, indent=4))
+        logger.debug("Payload received with headers:\n %s", dict(request.headers))
+        logger.debug("Payload contents:\n %s", json.dumps(request.json, indent=4))
         payload = hook.verify_incoming_webhook(request.headers, request.data)
         if not payload:
             return Response("Unauthorized", 403)
@@ -41,23 +45,58 @@ def clockbridge():
         # From here on out is just kludge code to make this work because I'm bored of manually entering data
         urllib3.disable_warnings(category=urllib3.exceptions.InsecureRequestWarning)
         now = datetime.now().astimezone()
-        index_name = f"test-{now.strftime('%Y-%m')}"
+        index_name = f"{config.elastic_creds['index_prefix']}-{now.strftime('%Y-%m')}"
         payload['@timestamp'] = now.strftime('%Y-%m-%dT%H:%M:%S%z')
         pwd = config.elastic_creds['password'].decode().strip()
-        
-        if hook.action == "TIME_ENTRY_DELETED":
-            r = delete_doc(config.elastic_creds['url'], index_name, payload['id'], config.elastic_creds['username'], pwd)
 
-        elif hook.action == "TIME_ENTRY_UPDATED":
-            r = delete_doc(config.elastic_creds['url'], index_name, payload['id'], config.elastic_creds['username'], pwd)
-            r = create_doc(config.elastic_creds['url'], index_name, payload['id'], config.elastic_creds['username'], pwd, payload)
-        else:
-            r = create_doc(config.elastic_creds['url'], index_name, payload['id'], config.elastic_creds['username'], pwd, payload)
-        return payload
+        health = requests.get(
+                    f"{config.elastic_creds['url']}_cluster/health?wait_for_status=yellow&timeout=50s",
+                    auth=(config.elastic_creds['username'], pwd),
+                    verify=not config.elastic_creds['insecure'],
+                    headers={"Content-Type": "application/json"},
+                    timeout=50
+                    )
+        logger.info("Elasticsearch health check returned %s", health.json()['status'])
+
+        if health.ok:
+            logger.info("Elasticsearch endpoint up, pushing data...")
+            if hook.action == "TIME_ENTRY_DELETED":
+                r = delete_doc(url=config.elastic_creds['url'],
+                               index=index_name,
+                               doc_id=payload['id'],
+                               user=config.elastic_creds['username'],
+                               pwd=pwd
+                               )
+
+            elif hook.action == "TIME_ENTRY_UPDATED":
+                r = delete_doc(url=config.elastic_creds['url'],
+                               index=index_name,
+                               doc_id=payload['id'],
+                               user=config.elastic_creds['username'],
+                               pwd=pwd
+                               )
+                r = create_doc(url=config.elastic_creds['url'],
+                               index=index_name,
+                               doc_id=payload['id'],
+                               user=config.elastic_creds['username'],
+                               pwd=pwd,
+                               data=payload
+                               )
+            else:
+                r = create_doc(url=config.elastic_creds['url'],
+                               index=index_name,
+                               doc_id=payload['id'],
+                               user=config.elastic_creds['username'],
+                               pwd=pwd,
+                               data=payload
+                               )
+            return r
+
+        raise requests.exceptions.HTTPError(health.content)
 
     except Exception:
         return Response(503)
-    
+
 def delete_doc(url, index, doc_id, user, pwd, verify_ssl=False):
     try:
         r = requests.delete(f"{url}{index}/_doc/{doc_id}",
@@ -65,25 +104,31 @@ def delete_doc(url, index, doc_id, user, pwd, verify_ssl=False):
                         verify=verify_ssl,
                         headers={"Content-Type": "application/json"},
                         timeout=10
-                    )    
-        logging.info("Deleted from Elasticsearch:\n %s", r.content)
-        return r.content
+                    )
+        if r.ok:
+            logger.info("Deleted existing document from Elasticsearch:\n %s", r.content)
+            return r.content
+        else:
+            raise requests.exceptions.HTTPError(r.content)
     except Exception as e:
-        logging.exception("Unable to delete document from Elasticsearch\n %s", e)
+        logger.exception("Unable to delete document from Elasticsearch\n %s", e)
 
-def create_doc(url, index, doc_id, user, pwd, payload, verify_ssl=False):
+def create_doc(url, index, doc_id, user, pwd, data, verify_ssl=False):
     try:
         r = requests.post(f"{url}{index}/_create/{doc_id}",
-                        data=json.dumps(payload),
+                        data=json.dumps(data),
                         auth=(user, pwd),
                         verify=verify_ssl,
                         headers={"Content-Type": "application/json"},
                         timeout=10
                     )
-        logging.info("Created from Elasticsearch:\n %s", r.content)
-        return r.content
+        if r.ok:
+            logger.info("Created new document in Elasticsearch:\n %s", r.content)
+            return r.content
+        else:
+            raise requests.exceptions.HTTPError(r.content)
     except Exception as e:
-        logging.exception("Unable to create document in Elasticsearch\n %s", e)
+        logger.exception("Unable to create document in Elasticsearch\n %s", e)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000, host='0.0.0.0')
